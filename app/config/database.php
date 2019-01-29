b224ebfc1f89a033daabbb96872d0976bd594e4e
Franco Jesus Mamani Pozo <mamanipozofrancojesus@gmail.com>
2019-01-29 19:07:25 -0400

    depuración de planta docente

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'cealm2',
+			'database'  => 'cealm',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
